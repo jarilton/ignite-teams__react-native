2da5a8c58e2767db354ec4d7042c3281dcd319dd
Jarilton Junior (Jamal) <jariltonpereira02@hotmail.com>
2023-01-18 00:02:53 -0300

    criado tipagem para usar o tema

--- src/screens/Groups/styles.ts
+++ src/screens/Groups/styles.ts
@@ -2,13 +2,14 @@ import styled from "styled-components/native";
 
 export const Container = styled.View`
   flex: 1;
-  background-color: #000;
+  background-color: ${({ theme }) => theme.colors.gray_600};
+  padding: 24px;
   align-items: center;
   justify-content: center;
 `;
 
 export const Title = styled.Text`
-  color: #fff;
+  color: ${({ theme }) => theme.colors.white};
   font-size: 32px;
   font-weight: bold;
 `;
