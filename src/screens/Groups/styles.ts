3dff29319b4f8afb45b477229622ce09be2f7f41
Jarilton Junior (Jamal) <jariltonpereira02@hotmail.com>
2023-01-18 22:43:02 -0300

    criado componente header

--- src/screens/Groups/styles.ts
+++ src/screens/Groups/styles.ts
@@ -4,12 +4,4 @@ export const Container = styled.View`
   flex: 1;
   background-color: ${({ theme }) => theme.colors.gray_600};
   padding: 24px;
-  align-items: center;
-  justify-content: center;
-`;
-
-export const Title = styled.Text`
-  color: ${({ theme }) => theme.colors.white};
-  font-size: 32px;
-  font-weight: bold;
 `;
