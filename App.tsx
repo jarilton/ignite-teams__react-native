14b4ff3e9a4ecf1858416915feda8a409f408cb8
Jarilton Junior (Jamal) <jariltonpereira02@hotmail.com>
2023-01-18 22:28:28 -0300

    Criado componente de loading

--- App.tsx
+++ App.tsx
@@ -1,15 +1,15 @@
-import { ActivityIndicator } from "react-native"
 import { ThemeProvider } from "styled-components";
 import { Groups } from "@screens/Groups";
 import theme from "./src/theme";
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from "@expo-google-fonts/roboto"
+import { Loading } from "@components/Loading";
 
 export default function App() {
   const [FontsLoaded] = useFonts([Roboto_400Regular, Roboto_700Bold]);
 
   return (
     <ThemeProvider theme={theme}>
-      { FontsLoaded ? <Groups /> : <ActivityIndicator /> }
+      { FontsLoaded ? <Groups /> : <Loading /> }
     </ThemeProvider>
   );
 }
