0762c9ba265072a8b020f3135864194d1333a0a6
Jarilton Junior (Jamal) <jariltonpereira02@hotmail.com>
2023-01-18 21:49:29 -0300

    fontes personalizadas

--- App.tsx
+++ App.tsx
@@ -1,11 +1,15 @@
+import { ActivityIndicator } from "react-native"
+import { ThemeProvider } from "styled-components";
 import { Groups } from "@screens/Groups";
 import theme from "./src/theme";
-import { ThemeProvider } from "styled-components";
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from "@expo-google-fonts/roboto"
 
 export default function App() {
+  const [FontsLoaded] = useFonts([Roboto_400Regular, Roboto_700Bold]);
+
   return (
     <ThemeProvider theme={theme}>
-      <Groups />
+      { FontsLoaded ? <Groups /> : <ActivityIndicator /> }
     </ThemeProvider>
   );
 }
