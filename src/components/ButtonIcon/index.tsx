c2ac515da4d0ab91adbc3060de9ed8860fbc4664
Jarilton Junior (Jamal) <jariltonpereira02@hotmail.com>
2023-01-23 23:54:50 -0300

    Deixando o icone do botão dinâmico

--- src/components/ButtonIcon/index.tsx
+++ src/components/ButtonIcon/index.tsx
@@ -1,12 +1,17 @@
 import { TouchableOpacityProps } from "react-native";
+import { MaterialIcons } from "@expo/vector-icons";
+
 import * as S from "./styles";
 
-type IProps = TouchableOpacityProps & {};
+type IProps = TouchableOpacityProps & {
+  icon: keyof typeof MaterialIcons.glyphMap;
+  type?: S.ButtonIconTypeStyleProps;
+};
 
-export function ButtonIcon({}: IProps) {
+export function ButtonIcon({ icon, type = "primary", ...rest }: IProps) {
   return (
-    <S.Container>
-      <S.Icon name="home" type="primary" />
+    <S.Container {...rest}>
+      <S.Icon name={icon} type={type} />
     </S.Container>
   );
 }
