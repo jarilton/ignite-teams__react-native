ce62458698f78d33e37b703fc7aaca47113935f7
Jarilton Junior (Jamal) <jariltonpereira02@hotmail.com>
2023-01-25 23:57:28 -0300

    Navegando pela aplicação

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,15 +1,22 @@
 import * as S from "./styles";
 import LogoImg from "@assets/logo.png";
+import { useNavigation } from "@react-navigation/native";
 
 type IProps = {
   ShowBackButton?: boolean;
 };
 
 export function Header({ ShowBackButton = false }: IProps) {
+  const navigation = useNavigation();
+
+  function handleBack() {
+    navigation.goBack();
+  }
+
   return (
     <S.Container>
       {ShowBackButton && (
-        <S.BackButton>
+        <S.BackButton onPress={handleBack}>
           <S.BackIcon />
         </S.BackButton>
       )}
--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -2,9 +2,16 @@ import { Button } from "@components/Button";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { Input } from "@components/Input";
+import { useNavigation } from "@react-navigation/native";
 import * as S from "./styles";
 
 export function NewGroup() {
+  const navigation = useNavigation();
+
+  function handlePlayers() {
+    navigation.navigate("Players", { group: "new" });
+  }
+
   return (
     <S.Container>
       <Header ShowBackButton />
@@ -16,7 +23,11 @@ export function NewGroup() {
           subtitle="Crie a turma para adicionar as pessoas"
         />
         <Input placeholder="Nome da turma" />
-        <Button title="Criar" style={{ marginTop: 20 }} />
+        <Button
+          title="Criar"
+          style={{ marginTop: 20 }}
+          onPress={handlePlayers}
+        />
       </S.Content>
     </S.Container>
   );
